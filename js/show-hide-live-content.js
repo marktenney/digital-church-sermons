7de18cb1de00814fb9d8cb4f402083ec65e367e3
Mark Tenney <mark@dgtl.church>
2019-09-23 16:29:41 -0400

    Update show-hide-live-content.js

--- js/show-hide-live-content.js
+++ js/show-hide-live-content.js
@@ -1,13 +1,10 @@
-function test(){
-  var date=new Date();
-  if(date.getDay()===0&& date.getHours()+1 >=9 && date.getMinutes()+1 >=45)
-  { //9:45-11:30AM
-    $(".on-air").dialog();
-    clearInterval(iId);
-  }
-elseif(date.getDay()===0&& date.getHours()+1 == 11 && date.getMinutes()+1 <= 30)
-{
-$(".on-air").dialog();
-clearInterval(iId);
-}
+var Now = new Date(),
+  CurrentDay = Now.getDay(),
+  OnAirTime = new Date(Now.getFullYear(), Now.getMonth(), Now.getDate(), 10, 45), //Set OnAirTime
+  OffAirTime = new Date(Now.getFullYear(), Now.getMonth(), Now.getDate(), 12, 30), //Set OffAirTime
+  OnAir = (Now.getTime() > OnAirTime.getTime() && Now.getTime() < OffAirTime.getTime());
+
+if (CurrentDay == 0 && OnAir) { //If it's Sunday and you're OnAir
+    $('.on-air').show(); // Show .on-air classes
+	$('.off-air').hide(); // Hide .off-air classes
 }
